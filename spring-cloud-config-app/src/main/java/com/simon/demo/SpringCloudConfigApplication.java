4e72b19993530838e538f3c12f4d5e9f0e95b9d5
Simon <seeding_xu@163.com>
2019-11-07 22:07:10 +0800

    cloud-configserver init 。暂时存在的问题：A default binder has been requested, but there is no binder available。

---
+++ spring-cloud-config-app/src/main/java/com/simon/demo/SpringCloudConfigApplication.java
@@ -0,0 +1,19 @@
+package com.simon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @ClassName SpringCloudConfigApplication
+ * @Description springCloud 的启动类
+ * @Author Simon
+ * @Date 2019/11/7   8:44 下午
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigApplication.class, args);
+    }
+}
